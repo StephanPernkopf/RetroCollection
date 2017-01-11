666f7607f657705e301deb856de60a3e455d673a
StephanPernkopf <stephan.pernkopf@outlook.com>
2017-01-11 11:05:11 +0100

    Add mathLib.js

--- Snake/index.js
+++ Snake/index.js
@@ -108,35 +108,3 @@ function randomColorString() {
 
 	return "rgb(" + r + "," + g + "," + b + ")";
 }
-
-function random(min, max) {
-	if (max == undefined) {
-		// from 0 to max --> argument min is max
-		return Math.random() * min;
-	} else {
-		// Random value between min to max
-		return Math.random() * (max - min) + min;
-	}
-}
-
-class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	distTo(p) {
-		if (p) {
-			if (typeof Math.hypot === "function") {
-				return Math.hypot(p.x - this.x, p.y - this.y);
-			} else {
-				// maybe provide another implementation
-			}
-		}
-	}
-
-	copy() {
-		return new Point(this.x, this.y);
-	}
-
-}
